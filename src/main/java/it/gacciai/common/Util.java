6c84d67bbe389f2df441ca86fd74147fa546a860
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-03 15:48:33 +0200

    Lecture 02.08

--- src/main/java/it/gacciai/common/Util.java
+++ src/main/java/it/gacciai/common/Util.java
@@ -3,6 +3,8 @@ package it.gacciai.common;
 import com.github.javafaker.Faker;
 import org.reactivestreams.Subscriber;
 
+import java.time.Duration;
+
 public class Util {
 
     private static final Faker faker = Faker.instance();
@@ -17,6 +19,14 @@ public class Util {
 
     public static Faker faker() {
         return faker;
+    }
+
+    public static void sleepSeconds(int seconds) {
+        try {
+            Thread.sleep(Duration.ofSeconds(seconds));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
---
+++ src/main/java/it/gacciai/sec02/Lec08MonoFromFuture.java
@@ -0,0 +1,49 @@
+package it.gacciai.sec02;
+
+import it.gacciai.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+/*
+    If you have a CompletableFuture already, then we can convert that into a Mono
+ */
+public class Lec08MonoFromFuture {
+
+    public static final Logger log = LoggerFactory.getLogger(Lec08MonoFromFuture.class);
+
+    public static void main(String[] args) {
+        log.info("method main starts");
+
+        //NOT LAZY! This will execute without the subscriber!!!!
+        Mono.fromFuture(getName());
+
+        //LAZY! This will execute only if there is a subscriber
+        Mono.fromFuture(() -> getName())
+                .subscribe(Util.subscriber("subscriber2"))
+        ;
+
+        //LAZY! AlSO this will execute only if there is a subscriber
+        Mono.fromFuture(Lec08MonoFromFuture::getName)
+                .subscribe(Util.subscriber("subscriber3"))
+        ;
+
+        log.info("wating for async completion");
+        Util.sleepSeconds(3);
+
+        log.info("method main ends");
+    }
+
+     private static CompletableFuture<String> getName() {
+        return CompletableFuture.supplyAsync(() -> {
+            Util.sleepSeconds(1);
+            log.info("generating name...");
+            Util.sleepSeconds(1);
+            log.info("generating name done!");
+            return Util.faker().name().firstName();
+        });
+     }
+
+}
