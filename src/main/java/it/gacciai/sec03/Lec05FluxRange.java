c2e0457d8d3f00ffe96ed7816da49eea822c4501
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-04 21:20:53 +0200

    Lecture 03.05 FluxRange

---
+++ src/main/java/it/gacciai/sec03/Lec05FluxRange.java
@@ -0,0 +1,29 @@
+package it.gacciai.sec03;
+
+import it.gacciai.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+public class Lec05FluxRange {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec05FluxRange.class);
+
+    public static void main(String[] args) {
+
+        Flux.range(1, 10)
+                .subscribe(Util.subscriber());
+
+        //Assignment generate 10 random fake names
+
+        Flux.range(1, 10)
+                .map(i -> i + " " + Util.faker().name().firstName())
+                .subscribe(Util.subscriber());
+
+        Flux.range(1, 10)
+                .map(i -> i + " " + Util.faker().hobbit().quote())
+                .subscribe(Util.subscriber());
+
+    }
+
+}
