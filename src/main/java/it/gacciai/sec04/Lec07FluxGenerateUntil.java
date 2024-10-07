50e21eee67186016132dfa61b8fd082bde943fa6
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-07 15:03:34 +0200

    Lecture 04.07 Assignment

---
+++ src/main/java/it/gacciai/sec04/Lec07FluxGenerateUntil.java
@@ -0,0 +1,15 @@
+package it.gacciai.sec04;
+
+import it.gacciai.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec07FluxGenerateUntil {
+
+    public static void main(String[] args) {
+        Flux.<String>generate(synchronousSync -> {
+                    synchronousSync.next(Util.faker().country().name());
+                })
+                .takeUntil(country -> "canada".equalsIgnoreCase(country))
+                .subscribe(Util.subscriber());
+    }
+}
