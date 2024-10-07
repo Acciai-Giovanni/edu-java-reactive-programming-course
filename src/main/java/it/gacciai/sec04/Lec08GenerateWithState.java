d89ac0a1a83fd29f06acc50327016533f3aaf7de
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-07 15:30:59 +0200

    Lecture 04.08 GenerateWithState - Improved Impl.

--- src/main/java/it/gacciai/sec04/Lec08GenerateWithState.java
+++ src/main/java/it/gacciai/sec04/Lec08GenerateWithState.java
@@ -9,6 +9,26 @@ public class Lec08GenerateWithState {
 
     public static void main(String[] args) {
 
+        Flux.<String,Integer>generate(
+                        () -> 0,
+                        (counter, sink) -> {
+                            var country = Util.faker().country().name();
+                            sink.next(country);
+
+                            if (counter + 1 == 10 || country.equalsIgnoreCase("canada")) {
+                                sink.complete();
+                            }
+
+                            return counter + 1;
+                        },
+                        counter -> System.out.println("counter: " + counter)
+
+                )
+                .subscribe(Util.subscriber());
+
+    }
+
+    private static void implementation1() {
         AtomicInteger counter = new AtomicInteger();
         Flux.generate(synchronousSink -> {
                     var country = Util.faker().country().name();
@@ -21,6 +41,5 @@ public class Lec08GenerateWithState {
                 })
                 // .take(50)
                 .subscribe(Util.subscriber());
-
     }
 }
