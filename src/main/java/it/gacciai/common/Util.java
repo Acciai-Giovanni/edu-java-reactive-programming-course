4fcf936019451f58654d95ae5ef4a0e87dd1e06a
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-17 15:04:27 +0200

    lecture 09.05 Merge

--- src/main/java/it/gacciai/common/Util.java
+++ src/main/java/it/gacciai/common/Util.java
@@ -2,11 +2,16 @@ package it.gacciai.common;
 
 import com.github.javafaker.Faker;
 import org.reactivestreams.Subscriber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.function.UnaryOperator;
 
 public class Util {
 
+    private static final Logger log = LoggerFactory.getLogger(Util.class);
     private static final Faker faker = Faker.instance();
 
     public static <T> Subscriber<T> subscriber() {
@@ -35,6 +40,13 @@ public class Util {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static <T> UnaryOperator<Flux<T>> fluxLogger(String name){
+        return flux -> flux
+                .doOnSubscribe(s -> log.info("subscribing to {}", name))
+                .doOnCancel(() -> log.info("cancelling {}", name))
+                .doOnComplete(() -> log.info("{} completed", name));
     }
 
 }
