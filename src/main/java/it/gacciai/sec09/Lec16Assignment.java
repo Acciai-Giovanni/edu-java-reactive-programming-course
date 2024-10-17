77f81ab115bb5a9e59ba6109c5a7c49122cc0bc0
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-17 19:29:49 +0200

    lecture 09.16 Assignment My Implementation

--- src/main/java/it/gacciai/sec09/Lec16Assignment.java
+++ src/main/java/it/gacciai/sec09/Lec16Assignment.java
@@ -1,6 +1,8 @@
 package it.gacciai.sec09;
 
-import it.gacciai.sec09.applications.Order;
+import it.gacciai.common.Util;
+import it.gacciai.sec09.applications.*;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,12 +12,26 @@ import java.util.List;
  */
 public class Lec16Assignment {
 
-    record UserInformation(Integer userId, String username, Integer balance, List<Order> orders) {
+    public record UserInformation(Integer userId, String username, Integer balance, List<Order> orders) {
     }
 
     public static void main(String[] args) {
 
 
+        UserService.getAllUsers()
+                .flatMap(Lec16Assignment::getUserInformation)
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(5);
+    }
+
+
+    public static Mono<UserInformation> getUserInformation(User user) {
+        return Mono.zip(
+                        PaymentService.getUserBalance(user.id()),
+                        OrderService.getUserOrders(user.id()).collectList()
+                )
+                .map(t -> new UserInformation(user.id(), user.username(), t.getT1(), t.getT2()));
     }
 
 }
